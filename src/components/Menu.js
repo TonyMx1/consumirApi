c9772064621bdbeccaa9f6106aa609c9d75274d4
Antonio <toniiserrano@gmail.com>
2024-02-04 19:52:18 -0600

    second commit

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -15,7 +15,7 @@ export function Menu(){
           <a className="nav-link active" aria-current="page" href="/contador">Contador</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/texto">Texto</a>
+          <a className="nav-link active" aria-current="page" href="/productos">Productos</a>
         </li>
       </ul>
       <form className="d-flex" role="search">
